f52aaf75f98f382776ee61f739b96fba083f9a38
qtira <176803464+qtyra@users.noreply.github.com>
2024-07-30 12:33:41 -0500

    feat: multiple sessions per cwd and git branch.

--- README.md
+++ README.md
@@ -4,7 +4,6 @@ smart-persistence.nvim is a small neovim plugin that uses the global working dir
 
 ## Contents
 
-- [Rationale](#rationale)
 - [Features](#features)
 - [Installation](#installation)
 - [Configuration](#configuration)
@@ -12,40 +11,12 @@ smart-persistence.nvim is a small neovim plugin that uses the global working dir
 - [Acknowledgements](#acknowledgements)
 - [Other plugins](#other-plugins)
 
-## Rationale
-
-**There are a lot of auto-session plugins, why another one, how is this different?**
-
-All the auto-session plugins I've tried use the window/tab local directory (`vim.fn.getcwd()` or `vim.uv.cwd()`) as the session filename. To illustrate why this can be problematic, consider this case:
-
-1. Open neovim from /foo, example: `nvim test.txt`.
-2. Open a new tab with `:tabnew` and change the directory with `:tcd bar` to /foo/bar.
-3. Close neovim with `:xa`, the directory returned by `vim.fn.getcwd()` and `vim.uv.cwd()` will be /foo/bar.
-4. The auto-session plugin saves the session as something like `$SESSIONDIR/%foo%bar.vim`.
-5. Reopen neovim, the plugin will either:
-    - Restore an old session
-    - Restore no session
-
-Both happen because the directory was saved with an tab/window local directory.
-
-**Why use tabs with different directories? Are you trying to use tabs as workspaces?**
-
-While you can use tabs and `tcd` to recreate workspaces, it's often easier to just open a new neovim instance. My use case is related but slightly different:
-
-1. I have a directory and a project under that directory, let's say /foo and /foo/project. /foo contains valuable information like books, notes and related projects. I want to share buffers and modify content from both /foo and /fooproject without constantly switching between two OS windows.
-2. Fuzzy finders like [fzf-lua](https://github.com/ibhagwan/fzf-lua) or [telescope](https://github.com/nvim-telescope/telescope.nvim) use the current working directory to search for files or call grep, plugins like [neogit](https://github.com/NeogitOrg/neogit) expect a .git directory, which may not exist in /foo or its parent directory.
-
-The best approach to my workflow I've come up with is to use `:tcd`, but as I mentioned earlier, auto-session plugins don't work well with this setup, leading me to write this plugin.
-
-**How do you track the global working directory?**
-
-Just use `vim.fn.getcwd(-1, -1)` instead of `vim.fn.getcwd()`. Another solution, which I used in the very first implementation of this plugin, is to set an autocommand for `DirChanged` to track directory changes made by `:cd` while ignoring `:tcd` and `:lcd`.
-
 ## Features
 
 - Sessions are loaded and saved based on the global working directory instead of the window/tab one.
 - Different sessions are saved based on the git branch.
-- Support session restore at startup.
+- Multiple sessions per directory and git branch.
+- Support auto session restore
 
 ## Installation
 
@@ -69,6 +40,8 @@ require("smart-persistence").setup({
     auto_restore = false,
     -- Don't automatically save session in these directories
     excluded_dirs = { "~/Downloads" },
+    -- Maximum number of sessions stored per cwd and git branch.
+    max_sessions = 10,
 })
 ```
 
@@ -100,16 +73,20 @@ vim.keymap.set("n", "<leader>qr", function() require("smart-persistence").restor
 
 -- Don't auto save the this session. Alternatively, set a list of directories in `excluded_dirs`.
 vim.keymap.set("n", "<leader>qs", function() require("smart-persistence").stop() end)
+
+-- Select a session based on your cwd and git branch.
+vim.keymap.set("n", "<leader>qs", function() require("smart-persistence").select() end)
 ```
 
 All exported commands:
 
 - `:SmartPersistence restore`
 - `:SmartPersistence stop`
+- `:SmartPersistence select`
 
 ## Acknowledgements
 
-The code is based on [persistence.nvim](https://github.com/folke/persistence.nvim), if not outright copied. It was the auto-session plugin I used before making my own.
+The code is based on [persistence.nvim](https://github.com/folke/persistence.nvim). It was the auto-session plugin I used before making my own.
 
 ## Other plugins
 
--- lua/smart-persistence/init.lua
+++ lua/smart-persistence/init.lua
@@ -6,20 +6,22 @@ local M = {}
 ---@field dir string
 ---@field auto_restore boolean
 ---@field excluded_dirs string[]?
+---@field max_sessions number
 local defaults = {
     dir = vim.fn.stdpath("data") .. "/smart-persistence/",
     auto_restore = false,
     excluded_dirs = { "~/Downloads" },
+    max_sessions = 10,
 }
 
 ---@type SmartPersistence.Config
 local conf
 
---- Session path according to a directory.
----@param dir string
+--- Builds the session dir respecting git branches
 ---@return string path
-local function session_path(dir)
-    dir = dir:gsub("[\\/:]+", "%%")
+local function get_session_dir()
+    local cwd = vim.fn.getcwd(-1, -1)
+    local dir = conf.dir .. cwd:gsub("[\\/:]+", "%%")
     if vim.fn.isdirectory(".git") then
         local obj = vim.system({ "git", "branch", "--show-current" }, { text = true }):wait()
         local branch = vim.trim(obj.stdout)
@@ -27,18 +29,33 @@ local function session_path(dir)
             dir = dir .. "%%" .. branch:gsub("[\\/:]+", "%%")
         end
     end
-    return conf.dir .. dir .. ".vim"
+    vim.fn.mkdir(dir, "p")
+    return dir
+end
+
+local function get_session_file(file)
+    return vim.fs.joinpath(get_session_dir(), file) .. ".vim"
 end
 
 --- Filter valid buffers.
 ---@param bufs number[]
 ---@return number[] valid_bufs
-local function valid_buffers(bufs)
+local function get_valid_buffers(bufs)
     return vim.tbl_filter(function(b)
         return vim.bo[b].buftype == ""
             and vim.api.nvim_buf_get_name(b) ~= ""
             and not vim.tbl_contains({ "gitcommit", "gitrebase" }, vim.bo[b].filetype)
     end, bufs)
+end
+
+--- Sessions in the current directory
+--- @return string[]
+local function get_sessions()
+    local sessions = vim.fn.glob(get_session_file("*"), true, true)
+    table.sort(sessions, function(a, b)
+        return vim.uv.fs_stat(a).mtime.sec > vim.uv.fs_stat(b).mtime.sec
+    end)
+    return sessions
 end
 
 --- Auto restore session if conditions are met.
@@ -76,7 +93,7 @@ end
 ---@field impl fun()
 
 -- https://github.com/nvim-neorocks/nvim-best-practices?tab=readme-ov-file#speaking_head-user-commands
-local function set_commands()
+local function setup_commands()
     ---@type table<string, SmartPersistence.subcmd>
     local subcmds = {
         restore = {
@@ -84,6 +101,9 @@ local function set_commands()
         },
         stop = {
             impl = M.stop,
+        },
+        select = {
+            impl = M.select,
         },
     }
     local function cmd_fn(opts)
@@ -106,17 +126,20 @@ local function set_commands()
     })
 end
 
-local function set_leave_autocmd()
+local function save_at_exit()
     vim.api.nvim_create_autocmd("VimLeavePre", {
         group = vim.api.nvim_create_augroup("smart-persistence", { clear = true }),
         callback = function()
-            local cwd = vim.fn.getcwd(-1, -1)
-            if vim.list_contains(conf.excluded_dirs, cwd) then
+            if vim.list_contains(conf.excluded_dirs, vim.fn.getcwd(-1, -1)) then
                 return
             end
-            local buffers = valid_buffers(vim.api.nvim_list_bufs())
+            local sessions = get_sessions()
+            if #sessions >= conf.max_sessions then
+                vim.uv.fs_unlink(sessions[#sessions])
+            end
+            local buffers = get_valid_buffers(vim.api.nvim_list_bufs())
             if #buffers > 0 then
-                local file = session_path(cwd)
+                local file = get_session_file(vim.fn.localtime())
                 vim.cmd("mks! " .. vim.fn.fnameescape(file))
             end
         end,
@@ -134,16 +157,33 @@ end
 function M.setup(opts)
     init_config(opts)
     auto_restore_session()
-    set_commands()
-    set_leave_autocmd()
+    setup_commands()
+    save_at_exit()
 end
 
 --- Restore last session
 function M.restore()
-    local file = session_path(vim.fn.getcwd(-1, -1))
+    local file = get_sessions()[1]
     if vim.fn.filereadable(file) ~= 0 then
         vim.cmd("silent so " .. vim.fn.fnameescape(file))
     end
+end
+
+--- Select a session from the cwd
+function M.select()
+    local sessions = get_sessions()
+    vim.ui.select(sessions, {
+        prompt = "Select a session: ",
+        format_item = function(item)
+            local date = vim.fs.basename(item):match("%d+")
+            return vim.fn.strftime("%d-%m-%Y %H:%M:%S", date)
+        end,
+    }, function(file)
+        if file then
+            vim.cmd("%bd!")
+            vim.cmd("silent so " .. vim.fn.fnameescape(file))
+        end
+    end)
 end
 
 --- Don't save current session
